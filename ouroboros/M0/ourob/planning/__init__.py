6f57d6f4fc0096b21b24ceaa4b153da66cc82ae1
Abdus2023 <130234450+Abdus2023@users.noreply.github.com>
2026-09-08 07:00:29 +0100

    feat(m2.11): export planning history reconstruction

--- ouroboros/M0/ourob/planning/__init__.py
+++ ouroboros/M0/ourob/planning/__init__.py
@@ -3,6 +3,7 @@ from .bridge import PlanningBridge, PlanningBridgeResult
 from .compat import LegacyPlannerAdapter
 from .context import ContextFact, PlanningContext
 from .controller import PlanningController, PlanningCycleResult
+from .history import PlanningAttempt, PlanningHistory, reconstruct_planning_history
 from .llm import LLMPlanner
 from .model import PlanValidation, PlanViolation, PlanningRequest, PlanningResponse, ProposedAction
 from .parser import SCHEMA, parse_response
@@ -14,6 +15,7 @@ from .validator import PlanValidator, canonical_request, canonical_response, dig
 __all__ = [
     "PlanningBridge", "PlanningBridgeResult", "PlanningController", "PlanningCycleResult",
     "ContextFact", "PlanningContext", "LLMPlanner", "LegacyPlannerAdapter", "PlanningReplay",
+    "PlanningAttempt", "PlanningHistory", "reconstruct_planning_history",
     "PlanValidation", "PlanViolation", "PlanningRequest", "PlanningResponse", "ProposedAction",
     "SCHEMA", "parse_response", "Planner", "PlannerProvider", "UnavailableProvider",
     "PlanValidator", "canonical_request", "canonical_response", "digest", "request_digest",
