55ac58fc4490c6c33b37e1f2ae3c0aecc3b6e552
Abdus2023 <130234450+Abdus2023@users.noreply.github.com>
2026-09-08 07:09:52 +0100

    M2.15: export canonical planning state

--- ouroboros/M0/ourob/planning/__init__.py
+++ ouroboros/M0/ourob/planning/__init__.py
@@ -17,10 +17,12 @@ from .parser import SCHEMA, parse_response
 from .protocol import Planner
 from .provider import PlannerProvider, UnavailableProvider
 from .replay import PlanningReplay, canonical_context, context_digest, planning_envelope_digest
+from .state import planning_attempt_count, planning_state
 from .validator import PlanValidator, canonical_request, canonical_response, digest, request_digest
 
 __all__ = [
-    "PlanningBridge", "PlanningBridgeResult", "planning_history", "PlanningController", "PlanningCycleResult",
+    "PlanningBridge", "PlanningBridgeResult", "planning_history", "planning_attempt_count", "planning_state",
+    "PlanningController", "PlanningCycleResult",
     "PLANNING_ACCEPTED", "PLANNING_ATTEMPT_EVENTS", "PLANNING_FAILED",
     "ContextFact", "PlanningContext", "LLMPlanner", "LegacyPlannerAdapter", "PlanningReplay",
     "PlanningAttempt", "PlanningHistory", "PlanningHistoryIntegrityError", "reconstruct_planning_history",
