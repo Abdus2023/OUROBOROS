9d2571391d25c8a79462c33bdbfcb35d07d03ee1
Abdus2023 <130234450+Abdus2023@users.noreply.github.com>
2026-09-08 06:55:40 +0100

    refactor(m2.7): expose planner lifecycle controller

--- ouroboros/M0/ourob/planning/__init__.py
+++ ouroboros/M0/ourob/planning/__init__.py
@@ -2,6 +2,7 @@
 from .bridge import PlanningBridge, PlanningBridgeResult
 from .compat import LegacyPlannerAdapter
 from .context import ContextFact, PlanningContext
+from .controller import PlanningController, PlanningCycleResult
 from .llm import LLMPlanner
 from .model import PlanValidation, PlanViolation, PlanningRequest, PlanningResponse, ProposedAction
 from .parser import SCHEMA, parse_response
@@ -10,7 +11,7 @@ from .provider import PlannerProvider, UnavailableProvider
 from .validator import PlanValidator, canonical_request, canonical_response, digest, request_digest
 
 __all__ = [
-    "PlanningBridge", "PlanningBridgeResult",
+    "PlanningBridge", "PlanningBridgeResult", "PlanningController", "PlanningCycleResult",
     "ContextFact", "PlanningContext", "LLMPlanner", "LegacyPlannerAdapter",
     "PlanValidation", "PlanViolation", "PlanningRequest", "PlanningResponse", "ProposedAction",
     "SCHEMA", "parse_response", "Planner", "PlannerProvider", "UnavailableProvider",
